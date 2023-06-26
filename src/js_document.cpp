1fffb817841b93730201bf255556dad3571599c7
Jack.WSL <foxintango@yeah.net>
2023-06-26 22:13:32 +0800

    alpine

--- src/js_document.cpp
+++ src/js_document.cpp
@@ -39,7 +39,12 @@ using namespace foxintango;
 #define ES_file_line_length_max 2048
 
 using namespace foxintango;
-js_document::js_document() {
+js_document::js_document(const ASTContext* context){
+    ASTDocument::ASTDocument(context);
+}
+js_document::js_document(const ASTContext* context, const URL& url){
+    ASTDocument::ASTDocument(context);
+    load(url);
 }
 
 js_document::~js_document() {
