f316563dbcd4890fd69d86ba0fa730d6c2a774f2
Jack.WSL <foxintango@yeah.net>
2023-07-03 17:21:34 +0800

    alpine

---
+++ src/module.cpp
@@ -0,0 +1,24 @@
+#include "module.h"
+#include "js_document.h"
+
+Array<ASTElementInterface*> ast_symbols;
+
+es_language_js_module_Interface::es_language_js_module_Interface(){}
+es_language_js_module_Interface::~es_language_js_module_Interface(){}
+
+bool es_language_js_module_Interface::containClass(const char* name){
+    if(String("js_document" == name))return true;
+}
+virtual void* es_language_js_module_Interface::createObject(const char* name, const Model& model = Model()){
+
+}
+bool es_language_js_module_Interface::containVariable(const char* name) {
+    return false;
+}
+
+void* es_language_js_module_Interface::exportVariable(const char* name) {
+    AST_EXPORT(ast_map);
+    return 0;
+}
+
+es_language_js_module_Interface foxintangoAPI MODULE_INTERFACE_INSTANCE;
---
+++ src/module.h
@@ -0,0 +1,19 @@
+#include <libast/libast.h>
+#include <libmodule/libmodule.h>
+#include <libcpp/libcpp.h>
+using namespace foxintango;
+AST_SYMBOL(ast_map);
+class foxintangoAPI es_language_js_module_Interface :public ModuleInterface {
+public:
+    es_language_js_module_Interface();
+    virtual ~es_language_js_module_Interface();
+public:
+    virtual bool  containClass(const char* name);
+    virtual void* createObject(const char* name, const Model& model = Model());
+
+    virtual bool  containFunction(const char* name);
+    virtual void* exportFunction(const char* name);
+
+    virtual bool containVariable(const char* name);
+    virtual void* exportVariable(const char* name);
+};
