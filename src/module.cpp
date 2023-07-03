ecc4ca977d9db0b88095eec550e5da3cc828413c
Jack.WSL <foxintango@yeah.net>
2023-07-03 17:56:20 +0800

    alpine

--- src/module.cpp
+++ src/module.cpp
@@ -11,7 +11,7 @@ bool es_language_js_module_Interface::containClass(const char* name){
 }
 void* es_language_js_module_Interface::createObject(const char* name, const Model& model){
     if(String("js_document") == name){
-        const ModelElement* e = model["context"];
+        const ModelElement* e = model.subelementAt("context");
         void* context = 0;
         if (e) e->as(&context);
         return new js_document(static_cast<ASTContext*>(context));
