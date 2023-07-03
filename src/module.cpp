697faa723bd2694d408eb00b434429ef602e5cd1
Jack.WSL <foxintango@yeah.net>
2023-07-03 17:29:23 +0800

    alpine

--- src/module.cpp
+++ src/module.cpp
@@ -7,10 +7,12 @@ es_language_js_module_Interface::es_language_js_module_Interface(){}
 es_language_js_module_Interface::~es_language_js_module_Interface(){}
 
 bool es_language_js_module_Interface::containClass(const char* name){
-    if(String("js_document" == name))return true;
+    if(String("js_document") == name) return true;
 }
-virtual void* es_language_js_module_Interface::createObject(const char* name, const Model& model = Model()){
+void* es_language_js_module_Interface::createObject(const char* name, const Model& model){
+    if(String("js_document") == name) return new js_document();
 
+    return 0;
 }
 bool es_language_js_module_Interface::containVariable(const char* name) {
     return false;
