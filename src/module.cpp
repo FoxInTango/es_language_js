9ea7c2f8e637fce5f80bb2fd46a32a792b119262
Jack.WSL <foxintango@yeah.net>
2023-07-03 17:45:17 +0800

    alpine

--- src/module.cpp
+++ src/module.cpp
@@ -12,9 +12,9 @@ bool es_language_js_module_Interface::containClass(const char* name){
 void* es_language_js_module_Interface::createObject(const char* name, const Model& model){
     if(String("js_document") == name){
         const ModelElement* e = model["context"];
-        ASTContext* context = 0;
-        if (e) e->as(&(void*)context);
-        return new js_document(context);
+        void* context = 0;
+        if (e) e->as(&context);
+        return new js_document(static_cast<ASTContext*>(context));
     }
 
     return 0;
