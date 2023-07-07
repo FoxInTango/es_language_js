3784b859a7bc47f25216f0828f4be6299406679f
Jack.WSL <foxintango@yeah.net>
2023-07-07 16:59:48 +0800

    alpine

--- src/js_token.cpp
+++ src/js_token.cpp
@@ -2,7 +2,7 @@
 foxintango::Unicode is_mark(const foxintango::Unicode& code){
     if(code == es_token_mark_bracket_l[0] || 
        code == es_token_mark_bracket_r[0]){
-        return code
+        return code;
     }
 
     return 0;
